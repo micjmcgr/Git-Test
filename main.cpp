59d9863ed39c46c164bbe691088d6cc5da06d6d7
Michael McGrath <micjmcgr@ut.utm.edu>
2016-03-27 11:56:23 -0500

    Fourth commit: adding summation function to main.cpp.

--- main.cpp
+++ main.cpp
@@ -9,9 +9,20 @@
 
 using namespace std;
 
-int main() {
+int summation(int number);
 
-  cout << "Hello world!" << endl;
- 
+int main() {
+  int n;
+  cout << "Please enter an integer: " << endl;
+  cin >> n;
+  cout << summation(n) << endl;
+
  return 0;
+}
+
+int summation(int number){
+  int returnValue = 0;
+  for(int i = 0; i <= number; i++)
+    returnValue += i;
+  return returnValue;
 }
